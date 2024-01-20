f2809eedb6d285d89097b8001cdf71efc9119ae2
Jhann Quiambao <quiambao.jhann1@gmail.com>
2024-01-20 15:34:47 -0500

    navbar component

---
+++ client/src/components/about/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default About
---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Joblify</h1>
+    <p class="lead">Elevate Your Resume, Dominate The Market!</p>
+  </div>
+</div>
+  );
+};
+
+export default Home;
