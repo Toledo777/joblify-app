24ad2d8c90718ff9970519b1ed75624c5ea004a6
Jimbao1 <quiambao.jhann1@gmail.com>
2024-01-21 02:09:45 -0500

    finish chatbot component

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,38 +1,25 @@
 import React from 'react';
-import "./header.css";
+import "./Header.css";
 
 const Header = () => {
   return (
-    
-    <div class="header">
-      <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-          <img src="/joblify.png" alt="Joblify Logo" width="30" height="24"/>
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <div>
+      <nav className="navbar">
+      <ul className="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+      <ul className="nav-links">
+        <li><a href="/">Joblify</a></li>
+      </ul>
+      <ul className="nav-links">
+        <li><a href="/services">Services</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
     </div>
+    
   )
 }
 
-export default Header
+export default Header;
