70cee07a901e0cd294d4ffa043b348ac93dedc3e
unknown <jeremy.tang@dawsoncollege.qc.ca>
2024-01-20 22:36:44 -0500

    add drag and drop functionality and file picker

--- client/src/DropFile.jsx
+++ client/src/DropFile.jsx
@@ -1,10 +1,11 @@
 import { useState, useRef } from "react";
 import './DropFile.css';
+import uploadIcon from ".\\assets\\cloud_upload_icon.png"
 
 function DropFile() {
   const [dragText, setDragText] = useState("Drag & Drop");
   const [file, setFile] = useState();
-  const inputFile = useRef(null) 
+  const inputFile = useRef(null)
 
   function handleDragEnter(e) {
     e.preventDefault();
@@ -27,32 +28,39 @@ function DropFile() {
     setFile(e.dataTransfer.files[0]);
     // console.log(file);
   }
-  function filePicker(e){
+  function filePicker(e) {
     e.preventDefault();
     inputFile.current.click();
   }
-  function tt(e){
+  function tt(e) {
     e.preventDefault();
+    console.log("inputFile");
     console.log(inputFile);
+    console.log("file");
+    console.log(file);
   }
   return (
-    <div className="container">
-      <button onClick={e => tt(e)}>test</button>
-      <h3>Upload Your File</h3>
-      <div className="drag-drop-area"
-        onDrop={e => handleDrop(e)}
-        onDragOver={e => handleDragOver(e)}
-        onDragEnter={e => handleDragEnter(e)}
-        onDragLeave={e => handleDragLeave(e)}
-      >
-        <div className="icon">
-          <i className="fas fa-images"></i>
+    <form>
+      <div className="container">
+        <button onClick={e => tt(e)}>test</button>
+        <h3>Upload Your Resume</h3>
+        <div className="drag-drop-area"
+          onDrop={e => handleDrop(e)}
+          onDragOver={e => handleDragOver(e)}
+          onDragEnter={e => handleDragEnter(e)}
+          onDragLeave={e => handleDragLeave(e)}
+        >
+          <div className="icon">
+            <img src={uploadIcon} alt="cloud upload icon"/>
+          </div>
+          <span className="header">{dragText}</span>
+          <span className="header"> or <span className="browse" onClick={e => filePicker(e)}>browse</span></span>
+          {/* <span className="header"> or </span> */}
+          <input type='file' ref={inputFile} style={{display: 'none'}}/>
         </div>
-        <span className="header">{dragText}</span>
-        <span className="header"> or <span className="button" onClick={e => filePicker(e)}>browse</span></span>
       </div>
-      <input type='file' id='file' ref={inputFile} style={{display: 'none'}}/>
-    </div>
+      <button className="submit" type="submit" onClick={e => e.preventDefault()}>Submit</button>
+    </form>
   );
 }
 
