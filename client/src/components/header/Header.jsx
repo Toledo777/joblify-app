38cee060a22c5a132dd364b122207a5dfbedd425
Jhann Quiambao <quiambao.jhann1@gmail.com>
2024-01-20 20:03:29 -0500

    chat component

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import "./header.css";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div class="header">
+        <ul class="left-links">
+          <li><a href="#">About</a></li>
+          <li><a href="#">Store</a></li>
+        </ul>
+        <ul class="right-links">
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Settings</a></li>
+        </ul>
+      </div>
   )
 }
 
